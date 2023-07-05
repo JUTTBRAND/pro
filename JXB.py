9051b899806e03d7f987607fceccdb38573ad3d2
MR AWAIS <106441016+JUTTBRAND@users.noreply.github.com>
2023-07-05 16:38:16 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -1,22 +1,22 @@
 #coding=utf-8
 import os, sys, platform
 #os.system('xdg-open https://facebook.com/groups/302474258349320/')
-os.system('rm -rf AWS.so')
+os.system('rm -rf aws.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf AWS.so')
+        os.system('rm -rf aws.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('AWS.so'):
-        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/AWS.cpython-311.so?raw=true -o AWS.so') 
-        import AWS
+    if not os.path.isfile('aws.so'):
+        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/aws.cpython-311.so?raw=true -o aws.so') 
+        import aws
     else:
-        import AWS
+        import aws
  
 elif bit == '32bit':
     print(" SORRY 32 BIT NOT WORKING ")
