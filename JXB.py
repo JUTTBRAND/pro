68ce48c908516658fa9d8a1f84c45dc3eaa2b701
MR AWAIS <106441016+JUTTBRAND@users.noreply.github.com>
2023-07-30 21:42:44 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -1,27 +1,22 @@
 #coding=utf-8
 import os, sys, platform
-#os.system('xdg-open https://facebook.com/groups/302474258349320/')
-os.system('rm -rf aws.so AWS2.so')
+os.system('xdg-open https://facebook.com/groups/302474258349320/')
+os.system('rm -rf aws1.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf aws.so AWS2.so')
+        os.system('rm -rf aws1.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('aws.so'):
-        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/aws.cpython-311.so?raw=true -o aws.so') 
-        import aws
+    if not os.path.isfile('aws1.so'):
+        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/aws1.cpython-311.so?raw=true -o aws1.so') 
+        import aws1
     else:
-        import aws
+        import aws1
  
-elif bit == '32bit':
-    if not os.path.isfile('AWS2.so'):
-        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/AWS2.cpython-311.so?raw=true -o AWS2.so') 
-        import AWS2
-    else:
-        import AWS2
+
     print(" CONGRATULATIONS YOUR PHONE SUPPORTED JUTTBRAND TOOL")
