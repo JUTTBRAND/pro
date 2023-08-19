e0476a64ffbcaf649557d26e6467cd2f57978c00
MR AWAIS <106441016+JUTTBRAND@users.noreply.github.com>
2023-08-19 12:01:20 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -13,7 +13,7 @@ except:
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('aws1.so'):
-        os.system('curl -L https://github.com/JUTTBRAND/JXB/blob/main/aws1.cpython-311.so?raw=true -o aws1.so') 
+        os.system('curl -L https://github.com/JUTTBRAND/SSB/blob/main/aws1.cpython-311.so?raw=true -o aws1.so') 
         import aws1
     else:
         import aws1
