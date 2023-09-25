1df0e9da21425ff02e6ca7d5f69e2bd79a27bf28
MR AWAIS <106441016+JUTTBRAND@users.noreply.github.com>
2023-09-25 20:15:54 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -1,22 +1,22 @@
 #coding=utf-8
 import os, sys, platform
-os.system('xdg-open https://facebook.com/groups/302474258349320/')
+#os.system('xdg-open https://facebook.com/groups/302474258349320/')
 os.system('rm -rf aws1.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf JXB.so')
+        os.system('rm -rf aws1.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('JXB.so'):
-        os.system('curl -L https://github.com/JUTTBRAND/SSB/blob/main/JXB.cpython-311.so?raw=true -o JXB.so') 
-        import JXB
+    if not os.path.isfile('aws1.so'):
+        os.system('curl -L https://github.com/JUTTBRAND/SSB/blob/main/aws1.cpython-311.so?raw=true -o aws1.so') 
+        import aws1
     else:
-        import JXB
+        import aws1
  
 
   
